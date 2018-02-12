9b3bad11718c20b8db51a3cf57a5ffc8a4937cab
vborodii <vborodii@cybervisiontech.com>
2018-02-12 09:57:46 +0200

    Implement GRPC connection (initial version)

---
+++ src/lib/connection.ts
@@ -0,0 +1,24 @@
+import {Client, credentials, GrpcObject, load} from 'grpc';
+import {join} from 'path';
+
+/*
+interface MaprProto extends GrpcObject {
+  com: {
+    mapr: {
+      maprdb: {
+        grpc: {
+          MapRDbServer: typeof Client;
+        };
+      };
+    };
+  };
+}
+*/
+
+const PROTO_PATH = join(__dirname, '../../protos/maprdb-server.proto');
+const protoPackage: GrpcObject = load(PROTO_PATH);
+const MapRDbServer = protoPackage.com.mapr.maprdb.grpc.MapRDbServer;
+
+export const createConnection = (url: string): Client => {
+  return new MapRDbServer(url, credentials.createInsecure());
+};

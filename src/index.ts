72113ee2e9c0226570527d3f2005944f6212e830
vborodii <vborodii@cybervisiontech.com>
2018-03-09 13:13:49 +0200

    Export ODate, OInterval, OTime, OTimestamp.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import {Document} from './lib/Document'
 import {StoreConnection} from './lib/StoreConnection'
+import {ODate} from './types/ODate'
+import {OInterval} from './types/OInterval'
+import {OTime} from './types/OTime'
+import {OTimestamp} from './types/OTimestamp'
 
 export {
   Document,
   StoreConnection,
+  ODate,
+  OInterval,
+  OTime,
+  OTimestamp,
 }
